ca6df71a7b238e5f266e0a651bbacdd413a3fbdb
xxd158 <273292695@qq.com>
2022-06-26 11:44:39 +0800

    路由配置完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// 一级目录，不用懒加载
+import LayOut from '@/views/LayOut'
+// 懒加载
+const Home = () => import('@/views/Home')
+const Video = () => import('@/views/Video')
+const Question = () => import('@/views/Question')
+const My = () => import('@/views/My')
+const Login = () => import('@/views/Login')
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    component: LayOut,
+    redirect: '/home',
+    children: [
+      { path: 'home', component: Home },
+      { path: 'video', component: Video },
+      { path: 'question', component: Question },
+      { path: 'my', component: My }
+    ]
+  },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
---
+++ src/views/LayOut/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <router-view />
+
+    <van-tabbar route>
+      <van-tabbar-item replace to="/home" icon="home-o">
+        <template #icon>
+          <i class="toutiao toutiao-shouye"></i>
+        </template>
+        首页</van-tabbar-item
+      >
+      <van-tabbar-item replace to="/video" icon="search">
+        <template #icon> <i class="toutiao toutiao-shipin"></i> </template
+        >视频</van-tabbar-item
+      >
+      <van-tabbar-item replace to="/question" icon="search">
+        <template #icon> <i class="toutiao toutiao-wenda"></i> </template
+        >问答</van-tabbar-item
+      >
+      <van-tabbar-item replace to="/my" icon="search">
+        <template #icon> <i class="toutiao toutiao-wode"></i> </template
+        >我的</van-tabbar-item
+      >
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  created () { },
+  data () {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  filters: {},
+  components: {}
+}
+</script>
+
+<style scoped lang='less'>
+.toutiao{
+  font-size: 40px;
+}
+ /deep/ .van-tabbar-item__text{
+   font-size: 20px!important
+}
+</style>
